2f3f09079e1a0e598c6344817d3274098b863d5b
syedammarilyas111 <syedammarilyas@hotmail.com>
2021-01-18 21:24:12 +0500

    image add to alphabets

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ for (var i = 0; i<numberOfDrumButtons; i++){
   document.querySelectorAll(".drum")[i].addEventListener("click",handleClick);
 
   function handleClick(){
-    alert("I got clicked!");
+  var audio = new Audio("sounds/tom-1.mp3");
+  audio.play();
+
+  //   alert("I got clicked!");
   }
 }
